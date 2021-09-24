4d0472e7d01de1f4920b0ec9fdfad0a9b60dd6fe
supercrafter333 <justcoolstuff99@gmail.com>
2021-09-24 14:26:33 +0200

    Fixed #25

--- src/supercrafter333/theSpawn/theSpawn.php
+++ src/supercrafter333/theSpawn/theSpawn.php
@@ -57,11 +57,11 @@ class theSpawn extends PluginBase implements Listener
     /**
      * @var
      */
-    public $config;
+    public Config $config;
     /**
      * @var
      */
-    public mixed $msgCfg;
+    public Config $msgCfg;
     /**
      * @var
      */
