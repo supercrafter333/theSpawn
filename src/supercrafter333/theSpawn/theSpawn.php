71a6018b913868482c2d7c019942839874ca2e09
supercrafter333 <justcoolstuff99@gmail.com>
2022-04-07 20:38:16 +0200

    Next is v1.8.1-dev

--- src/supercrafter333/theSpawn/theSpawn.php
+++ src/supercrafter333/theSpawn/theSpawn.php
@@ -115,10 +115,10 @@ class theSpawn extends PluginBase
     /**
      * @var string
      */
-    public string $version = "1.8.0";
+    public string $version = "1.8.1-dev";
 
 
-    public const DEVELOPMENT_VERSION = false;
+    public const DEVELOPMENT_VERSION = true;
 
 
 
