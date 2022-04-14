053aa4e91715103075896e98095c3a527e2bdfd6
supercrafter333 <justcoolstuff99@gmail.com>
2022-04-14 21:25:01 +0200

    Release theSpawn v1.8.2

--- src/supercrafter333/theSpawn/theSpawn.php
+++ src/supercrafter333/theSpawn/theSpawn.php
@@ -115,10 +115,10 @@ class theSpawn extends PluginBase
     /**
      * @var string
      */
-    public string $version = "1.8.2-dev";
+    public string $version = "1.8.2";
 
 
-    public const DEVELOPMENT_VERSION = true;
+    public const DEVELOPMENT_VERSION = false;
 
 
 
